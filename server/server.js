8a0485a06dead0aa32122b24588d8d2f17be40c5
Kedirech <brackcarmony@gmail.com>
2017-01-27 13:11:11 -0700

    Make it into a simple webook tester

--- server/server.js
+++ server/server.js
@@ -1,26 +1,19 @@
 var express = require("express");
 var app = express();
-var serverConfig = require("./../server_config.js");
+var serverConfig = require("./server_config.js");
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
-var _ = require('underscore');
 var port = serverConfig.serverPort;
 var cors = require('cors');
 
 app.use(cors());
-app.use(express.static(__dirname+'/../public'));
 app.use(bodyParser.json());
 
-app.set('trust proxy', 1) // trust first proxy
-app.use(session({
-  secret: 'Whahaha Cookies For All',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}))
-
-require('./controllers/chatCtrl')(app);
+app.post('/webhook/test', function(req, res){
+  console.log(req.body);
+  console.log(Object.keys(req.body));
+})
 
 app.listen(port, function(){
   console.log("Listeing on port ", port);
