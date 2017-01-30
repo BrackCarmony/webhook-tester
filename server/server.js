16d3734c7c6e7512a363a6b3b0be13a57129aaaa
Brack Carmony <=>
2017-01-30 15:28:11 -0500

    Remove mongoose dep

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ var express = require("express");
 var app = express();
 var serverConfig = require("./server_config.js");
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
 var session = require('express-session');
 var port = serverConfig.serverPort;
 var cors = require('cors');
